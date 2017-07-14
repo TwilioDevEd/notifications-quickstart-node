8d1c19d09273f562987927f02711b2bcfa4c3747
Jeffrey Linwood <jlinwood@gmail.com>
2017-07-14 17:42:12 -0500

    Merged into SDK Starter

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  "TWILIO_ACCOUNT_SID": "", // Twilio Account SID from https://www.twilio.com/console
-  "TWILIO_AUTH_TOKEN": "", // Twilio Auth Token from https://www.twilio.com/console
-  "TWILIO_NOTIFICATION_SERVICE_SID": "", // Service SID from https://www.twilio.com/console/notify/services
-}
